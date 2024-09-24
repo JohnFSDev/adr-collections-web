3f0d077e7bf59bdf0a800199ccef78e8d7c39aa8
JohnFSDev <johnluis22@outlook.com>
2024-09-24 14:55:20 -0400

    Actualizacion del deploy y archivo astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://JohnFSDev.github.io/adr-collections-web/',
-  integrations: [tailwind()],
-  output: 'static'
+  integrations: [tailwind()]
 });
